b5716fd3fcdd125cb4ad406b34715f1da8ed5f24
IzzatHafizzuddin <92623319+IzzatHafizzuddin@users.noreply.github.com>
2023-05-17 15:39:37 +0800

    Add files via upload

---
+++ lib/pages/loadingPage.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../widget/logoDisplay.dart';
+import 'homePage.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Add a delay to simulate a loading process
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  var backgroundImage = 'assets/GreenBackground/newbackground.png';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: BoxDecoration(image: buildBackground()),
+      child: Center(child: LogoDisplay()),
+    ));
+  }
+
+  DecorationImage buildBackground() {
+    return DecorationImage(
+        image: AssetImage(backgroundImage), fit: BoxFit.fill);
+  }
+}
